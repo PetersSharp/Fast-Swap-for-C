45ef98b965b31998008bf5f84bfc5c222ad5b253
PetersSharp <cwdwshare@gmail.com>
2018-06-02 17:41:34 +0300

    Add files via upload

--- fswap.h
+++ fswap.h
@@ -19,66 +19,77 @@
 
 #define __swap_wrapper(A,B) __extension__ \
     (__builtin_choose_expr( \
-        __builtin_types_compatible_p(__typeof__(A), int[]), __swap(A, B, sizeof(A), sizeof(B)), \
+        __builtin_types_compatible_p(__typeof__(A), int[]), __swap_p(A, B, sizeof(A), sizeof(B)), \
             __builtin_choose_expr( \
-        __builtin_types_compatible_p(__typeof__(A), const int[]), __swap(A, B, sizeof(A), sizeof(B)), \
+        __builtin_types_compatible_p(__typeof__(A), const int[]), __swap_p(A, B, sizeof(A), sizeof(B)), \
             __builtin_choose_expr( \
-        __builtin_types_compatible_p(__typeof__(A), unsigned int[]), __swap(A, B, sizeof(A), sizeof(B)), \
+        __builtin_types_compatible_p(__typeof__(A), unsigned int[]), __swap_p(A, B, sizeof(A), sizeof(B)), \
             __builtin_choose_expr( \
-        __builtin_types_compatible_p(__typeof__(A), const unsigned int[]), __swap(A, B, sizeof(A), sizeof(B)), \
+        __builtin_types_compatible_p(__typeof__(A), const unsigned int[]), __swap_p(A, B, sizeof(A), sizeof(B)), \
             __builtin_choose_expr( \
-        __builtin_types_compatible_p(__typeof__(A), short[]), __swap(A, B, sizeof(A), sizeof(B)), \
+        __builtin_types_compatible_p(__typeof__(A), short[]), __swap_p(A, B, sizeof(A), sizeof(B)), \
             __builtin_choose_expr( \
-        __builtin_types_compatible_p(__typeof__(A), const short[]), __swap(A, B, sizeof(A), sizeof(B)), \
+        __builtin_types_compatible_p(__typeof__(A), const short[]), __swap_p(A, B, sizeof(A), sizeof(B)), \
             __builtin_choose_expr( \
-        __builtin_types_compatible_p(__typeof__(A), unsigned short[]), __swap(A, B, sizeof(A), sizeof(B)), \
+        __builtin_types_compatible_p(__typeof__(A), unsigned short[]), __swap_p(A, B, sizeof(A), sizeof(B)), \
             __builtin_choose_expr( \
-        __builtin_types_compatible_p(__typeof__(A), const unsigned short[]), __swap(A, B, sizeof(A), sizeof(B)), \
+        __builtin_types_compatible_p(__typeof__(A), const unsigned short[]), __swap_p(A, B, sizeof(A), sizeof(B)), \
             __builtin_choose_expr( \
-        __builtin_types_compatible_p(__typeof__(A), long[]), __swap(A, B, sizeof(A), sizeof(B)), \
+        __builtin_types_compatible_p(__typeof__(A), long[]), __swap_p(A, B, sizeof(A), sizeof(B)), \
             __builtin_choose_expr( \
-        __builtin_types_compatible_p(__typeof__(A), const long[]), __swap(A, B, sizeof(A), sizeof(B)), \
+        __builtin_types_compatible_p(__typeof__(A), const long[]), __swap_p(A, B, sizeof(A), sizeof(B)), \
             __builtin_choose_expr( \
-        __builtin_types_compatible_p(__typeof__(A), unsigned long[]), __swap(A, B, sizeof(A), sizeof(B)), \
+        __builtin_types_compatible_p(__typeof__(A), unsigned long[]), __swap_p(A, B, sizeof(A), sizeof(B)), \
             __builtin_choose_expr( \
-        __builtin_types_compatible_p(__typeof__(A), const unsigned long[]), __swap(A, B, sizeof(A), sizeof(B)), \
+        __builtin_types_compatible_p(__typeof__(A), const unsigned long[]), __swap_p(A, B, sizeof(A), sizeof(B)), \
             __builtin_choose_expr( \
-        __builtin_types_compatible_p(__typeof__(A), long long[]), __swap(A, B, sizeof(A), sizeof(B)), \
+        __builtin_types_compatible_p(__typeof__(A), long long[]), __swap_p(A, B, sizeof(A), sizeof(B)), \
             __builtin_choose_expr( \
-        __builtin_types_compatible_p(__typeof__(A), const long long[]), __swap(A, B, sizeof(A), sizeof(B)), \
+        __builtin_types_compatible_p(__typeof__(A), const long long[]), __swap_p(A, B, sizeof(A), sizeof(B)), \
             __builtin_choose_expr( \
-        __builtin_types_compatible_p(__typeof__(A), unsigned long long[]), __swap(A, B, sizeof(A), sizeof(B)), \
+        __builtin_types_compatible_p(__typeof__(A), unsigned long long[]), __swap_p(A, B, sizeof(A), sizeof(B)), \
             __builtin_choose_expr( \
-        __builtin_types_compatible_p(__typeof__(A), unsigned const long long[]), __swap(A, B, sizeof(A), sizeof(B)), \
+        __builtin_types_compatible_p(__typeof__(A), unsigned const long long[]), __swap_p(A, B, sizeof(A), sizeof(B)), \
             __builtin_choose_expr( \
-        __builtin_types_compatible_p(__typeof__(A), char*), __swap_p((void**)&A, (void**)&B), \
+        __builtin_types_compatible_p(__typeof__(A), char*), __swap_pp((void**)&A, (void**)&B), \
             __builtin_choose_expr( \
-        __builtin_types_compatible_p(__typeof__(A), const char*), __swap_p((void**)&A, (void**)&B), \
+        __builtin_types_compatible_p(__typeof__(A), const char*), __swap_pp((void**)&A, (void**)&B), \
             __builtin_choose_expr( \
-        __builtin_types_compatible_p(__typeof__(A), char[]), __swap(A, B, (sizeof(A) - 1), (sizeof(B) - 1)), \
+        __builtin_types_compatible_p(__typeof__(A), char[]), __swap_p(A, B, (sizeof(A) - 1), (sizeof(B) - 1)), \
             __builtin_choose_expr( \
-        __builtin_types_compatible_p(__typeof__(A), const char[]), __swap(A, B, (sizeof(A) - 1), (sizeof(B) - 1)), \
+        __builtin_types_compatible_p(__typeof__(A), const char[]), __swap_p(A, B, (sizeof(A) - 1), (sizeof(B) - 1)), \
             __builtin_choose_expr( \
-        __builtin_types_compatible_p(__typeof__(A), unsigned char*), __swap_p((void**)&A, (void**)&B), \
+        __builtin_types_compatible_p(__typeof__(A), unsigned char*), __swap_pp((void**)&A, (void**)&B), \
             __builtin_choose_expr( \
-        __builtin_types_compatible_p(__typeof__(A), const unsigned char*), __swap_p((void**)&A, (void**)&B), \
+        __builtin_types_compatible_p(__typeof__(A), const unsigned char*), __swap_pp((void**)&A, (void**)&B), \
             __builtin_choose_expr( \
-        __builtin_types_compatible_p(__typeof__(A), unsigned char[]), __swap(A, B, (sizeof(A) - 1), (sizeof(B) - 1)), \
+        __builtin_types_compatible_p(__typeof__(A), unsigned char[]), __swap_p(A, B, (sizeof(A) - 1), (sizeof(B) - 1)), \
             __builtin_choose_expr( \
-        __builtin_types_compatible_p(__typeof__(A), const unsigned char[]), __swap(A, B, (sizeof(A) - 1), (sizeof(B) - 1)), \
+        __builtin_types_compatible_p(__typeof__(A), const unsigned char[]), __swap_p(A, B, (sizeof(A) - 1), (sizeof(B) - 1)), \
             __builtin_choose_expr( \
-        __builtin_types_compatible_p(__typeof__(A), void*), __swap_p((void**)&A, (void**)&B), \
+        __builtin_types_compatible_p(__typeof__(A), void*), __swap_pp((void**)&A, (void**)&B), \
             __builtin_choose_expr( \
-        __builtin_types_compatible_p(__typeof__(A), const void*), __swap_p((void**)&A, (void**)&B), \
-    __swap(A, B, sizeof(*A), sizeof(*B)))))))))))))))))))))))))))))
+        __builtin_types_compatible_p(__typeof__(A), const void*), __swap_pp((void**)&A, (void**)&B), \
+            __builtin_choose_expr( \
+        __builtin_types_compatible_p(__typeof__(A), void*[]), __swap_ppp((void***)&A, (void***)&B), \
+            __builtin_choose_expr( \
+        __builtin_types_compatible_p(__typeof__(A), const void*[]), __swap_ppp((void***)&A, (void***)&B), \
+    __swap_p(A, B, sizeof(*A), sizeof(*B)))))))))))))))))))))))))))))))
 
 
-__attribute__((always_inline)) void __swap_p(void **a, void **b) {
+__attribute__((always_inline)) inline void __swap_ppp(void ***a, void ***b)
+{
+    void **c = *a;
+    *a = *b; *b = c;
+}
+
+__attribute__((always_inline)) inline void __swap_pp(void **a, void **b)
+{
     void *c = *a;
     *a = *b; *b = c;
 }
 
-__attribute__((always_inline)) void __swap(void *a, void *b, size_t na, size_t nb)
+__attribute__((always_inline)) inline void __swap_p(void *a, void *b, size_t na, size_t nb)
 {
     if ((!na) || (na != nb)) { return; }
 
